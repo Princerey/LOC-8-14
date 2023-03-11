ecf58f476fef93635c466a2a78f632e01dca68d2
ansh378 <126560283+ansh378@users.noreply.github.com>
2023-03-11 23:45:19 +0530

    Add files via upload

---
+++ question1(asgn-1).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int i;
+    for(i=1;i<=100;i++)
+    if(i%3==0){printf("Fizz\n");}
+    else if(i%5==0){printf("Buzz\n");}
+    else
+    printf("%d\n",i);
+    getch();
+    return 0;
+}
